9e92b87068221efc2a66a5349bf3240f6e6906e7
kathytransuwu <158067933+kathytransuwu@users.noreply.github.com>
2026-02-18 22:05:58 +0000

    Camera fixed
    Tied to framerate, possibly need to optimize or find issue?

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -35,7 +35,7 @@ public class CameraMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        
         Vector2 lookDelta = Controls.ReadValue<Vector2>();
 
         //Gets the movement data from the mouse, times by sensitivity and dependent on framerate with delta time.
@@ -44,10 +44,12 @@ public class CameraMovement : MonoBehaviour
 
         xRotation -= mouseY;
         //Clamp basically means it can't go any further down than -90f and not higher than MinCameraMovement
-        xRotation = Mathf.Clamp(xRotation, -90f, MinCameraMovement);
+        xRotation = Mathf.Clamp(xRotation, -MinCameraMovement, MinCameraMovement);
         //Only rotates on the X axis to avoid rotating the player body with the camera.
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
 
         PlayerBody.Rotate(Vector3.up * mouseX);
+
+        
     }
 }
